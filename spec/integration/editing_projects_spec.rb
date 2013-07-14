38bd6a9a457339c0e5767c59b22b15900b400d35
Laura Erskine <lrskne@gmail.com>
2013-07-13 20:05:00 -0700

    FeatureA - the end of the U in CRUD

--- spec/integration/editing_projects_spec.rb
+++ spec/integration/editing_projects_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
-feature "Editing Projectssss" do
-  scenario "Updating a project" do
-    Factory(:project, :name => "TextMate 2")
+feature "Editing Project" do
+  before do
+        Factory(:project, :name => "TextMate 2")
     visit "/"
     click_link "TextMate 2"
     click_link "Edit Project"
+  end
+  scenario "Updating a project" do
     fill_in "Name", :with => "TextMate 2 beta"
     click_button "Update Project"
     page.should have_content("Project has been updated.")
+  end
+  scenario "Updating a project with invalid attributes is bad" do
+    fill_in "Name", :with => ""
+    click_button "Update Project"
+    page.should have_content("Project has not been updated.")
   end
 end
